4db702b62007da24d767d6473ef6e3295c60478a
Stepan <stepan.d.orlov@mail.ru>
2023-06-16 21:19:21 +0300

    Task2

---
+++ Seminar_3/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая по заданному номеру четверти,
+ показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+
+Console.Write("Введите номер четверти от 1 до 4:");
+int a = int.Parse(Console.ReadLine());
+
+switch (a)
+{
+    case 1: 
+    Console.WriteLine("Диапозон x > 0, y > 0");
+    break;
+
+    case 2:
+    Console.WriteLine("Диапозон x < 0, y > 0");
+    break;
+
+    case 3: 
+    Console.WriteLine("Диапозон x < 0, y < 0");
+    break;
+
+    case 4:
+    Console.WriteLine("Диапозон x > 0, y < 0");
+    break;
+
+    default:
+    Console.WriteLine("Вы ввели значение не от 1 до 4");
+    break;
+}
