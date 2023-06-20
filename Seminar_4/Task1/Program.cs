fdc2e3106f2d10519f009aed5176b0d055c8960a
Stepan <stepan.d.orlov@mail.ru>
2023-06-20 22:05:16 +0300

    Task1,2 Seminar4

--- Seminar_4/Task1/Program.cs
+++ Seminar_4/Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 26: Напишите программу, которая принимает 
+на вход число и выдаёт количество цифр в числе.*/
+
+
+Console.WriteLine("Введите число: ");
+string user_input = Console.ReadLine();
+
+Console.WriteLine("Длина введёного числа " + user_input.Length + "символа"); 
+
+
+/*Console.WriteLine("Пожалуйста введите число: ");
+int user_input = int.Parse(Console.ReadLine());
+
+int getLength(int count){
+    int count = 0; 
+    while(user_input != 0)
+    {
+        user_input = user_input / 10;
+        count++;
+    }
+    return count;
+    Console.WriteLine($"Длина введёного числа = {count}");
+}*/
---
+++ Seminar_4/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Напишите программу, которая принимает на вход число N и 
+выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120*/
+
+int result = 1;
+
+Console.WriteLine("Введите число: ");
+int input_number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= input_number; i++)
+{
+    result *= i;
+    Console.WriteLine($"Промежуточный результат {result}"); 
+}
+
+    Console.WriteLine($"Производная всех чисел от 1 до {input_number} равняется {result}");
+
+/*
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int getFactorial(int number){
+
+    int result = 1;
+    for(int i = 1; i <= number; i ++)
+    {
+        result *= i;
+    }
+        return result;
+
+}
+ 
+Console.WriteLine(getFactorial(number));*/
