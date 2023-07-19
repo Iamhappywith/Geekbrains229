3e03230adc353e290b5ee6a615f5b282577ed8a0
Stepan <stepan.d.orlov@mail.ru>
2023-07-19 15:18:08 +0300

    Seminar_9HW

---
+++ Seminar_9/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Выведите натуральные числа от M до N*/
+
+Console.Write("Введите первое число: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numN = int.Parse(Console.ReadLine());
+
+Console.Write( numM + " " );
+
+int FillNatural(int M, int N)
+{
+    if (M == N) return N;
+    int temp = 1 + FillNatural(M, N-1);
+    Console.Write(" "+ N +" ");
+    return temp; 
+}
+
+FillNatural(numM, numN);
---
+++ Seminar_9/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая будет принимать на фход число и возвращать сумму его цифр */
+
+Console.Write("Input integer: ");
+int number = int.Parse(Console.ReadLine());
+
+int SumRecIntegers(int number)
+{
+    if (number == 0) return 0;
+    int temp = number % 10 + SumRecIntegers(number / 10);
+    return temp;   
+} 
+
+Console.WriteLine(SumRecIntegers(number));
